3d14ccaaa4cd84d783555c52c71aca6c9f7738b2
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-08-13 15:21:59 +0430

    Better user experience

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function setInfo(data) {
   cityInput.focus();
 }
 window.addEventListener("keydown", (e) => {
-  if (e.code == "Enter") {
+  if (e.code == "Enter" || e.code == "NumpadEnter") {
     getWeather();
   }
 });
